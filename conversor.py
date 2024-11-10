bbebccca2e29861c397a1af48c30de8109a2ea45
Lucas Nazário <lucaskenpachi57@gmail.com>
2024-11-10 12:03:50 -0300

    usando cores
    incrementando ansi

--- conversor.py
+++ conversor.py
@@ -1,3 +1,3 @@
 peso = float(input('Digite seu peso(Em quilogramas): '))
 libras = peso * 2.20462
-print('Seu peso é {:.2f}kg, em libras fica {:.2f}lbs'.format(peso,libras))
+print('\33[1;34m{}\33[m kg = \33[1;31m{:.2f}\33[m lbs'.format(peso,libras))
