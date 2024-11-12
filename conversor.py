5dd1107b476ee22cee8e4a9a774318033a9d5567
Lucas Nazário <lucaskenpachi57@gmail.com>
2024-11-12 17:30:13 -0300

    Incrementando peso
    Adição conversão de peso

--- conversor.py
+++ conversor.py
@@ -1,3 +1,9 @@
-peso = float(input('Digite seu peso(Em quilogramas): '))
+peso = float(input('Digite seu peso: '))
+altura = float(input('Digite sua altura: '))
+polegada = altura * 0.393701
 libras = peso * 2.20462
-print('\33[1;34m{}\33[m kg = \33[1;31m{:.2f}\33[m lbs'.format(peso,libras))
+if polegada > 12 and altura > 100:
+    conversaoaltura = altura/100
+    conversaopolegada= polegada/12
+print('\33[1;34m{:.2f}\33[m kg = \33[1;31m{:.2f}\33[m lbs'.format(peso,libras)) 
+print('\33[1;34m{:.2f}\33[m m = \33[1;31m{:.2f}\33[m in'.format(conversaoaltura,conversaopolegada)) 
